835cd5575ffd9899861327409aa7f5ec16f7fa8c
KitchenBoy <coleton.trenton@outlook.com>
2023-06-28 12:27:48 -0500

    Adding Checksum Stuffs

---
+++ Core/src/Utilities/Internal/ChecksumCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace LabFusion.Core.src.Utilities.Internal
+{
+    internal class ChecksumCalculator
+    {
+        public static string CalculateChecksum(string filePath)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                using (var fileStream = File.OpenRead(filePath))
+                {
+                    byte[] checksum = sha256.ComputeHash(fileStream);
+                    return BitConverter.ToString(checksum).Replace("-", string.Empty);
+                }
+            }
+        }
+    }
+}
