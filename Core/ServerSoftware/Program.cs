2b43dc7056fbb586eb9312b41632949222092b98
Checkerb0ard <Colehennessy1@gmail.com>
2023-09-05 16:07:09 -0700

    And so it begins
    this is going to be hell :P

---
+++ Core/ServerSoftware/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerSoftware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
